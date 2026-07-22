0036ec459708bc5061e6cbd0c85893de8bf13306
Maxime Merrien <maxime.merrien@atos.net>
2026-07-22 16:50:32 +0200

    chore(rector): update type coverage level to 24

--- rector.php
+++ rector.php
@@ -11,7 +11,7 @@ return RectorConfig::configure()
     ])
     // uncomment to reach your current PHP version
      ->withPhpSets()
-    ->withTypeCoverageLevel(23)
+    ->withTypeCoverageLevel(24)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0)
 ;
