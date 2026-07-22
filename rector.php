eddfb734e2adc229f303a590f8998176d6f8bba8
Maxime Merrien <maxime.merrien@atos.net>
2026-07-22 17:17:57 +0200

    chore(rector): update dead code level to 31

--- rector.php
+++ rector.php
@@ -12,6 +12,6 @@ return RectorConfig::configure()
     // uncomment to reach your current PHP version
      ->withPhpSets()
     ->withTypeCoverageLevel(50)
-    ->withDeadCodeLevel(30)
+    ->withDeadCodeLevel(31)
     ->withCodeQualityLevel(0)
 ;
